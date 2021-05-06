9e2b9d7e4db2981602e74112f538e10c75ec8fc5
Alexandre Gressier <alexandre@gressier.dev>
2021-05-06 19:19:33 +0200

    Implement a basic To-do List management app

--- front-end/web/todo-web-app/src/App.tsx
+++ front-end/web/todo-web-app/src/App.tsx
@@ -1,10 +1,73 @@
 import React, { Component } from "react"
 
-export default class App extends Component {
+interface ToDo {
+    text: string
+    isDone: boolean
+}
+
+
+interface AppState {
+    username: string
+    toDos: ToDo[]
+    newToDoText: string
+}
+
+export default class App extends Component<{}, AppState> {
+
+    constructor(props: {}) {
+        super(props)
+        this.state = {
+            username: "Alex",
+            toDos: [
+                { text: "Buy Flowers", isDone: false },
+                { text: "Get Shoes", isDone: false },
+                { text: "Collect Tickets", isDone: true },
+                { text: "Call Joe", isDone: false },
+            ],
+            newToDoText: ""
+        }
+    }
+
+    createNewTodo = () =>
+        this.state.newToDoText
+        && !this.state.toDos.find(toDo => toDo.text === this.state.newToDoText)
+        && this.setState({
+            toDos: [...this.state.toDos, { text: this.state.newToDoText, isDone: false }],
+            newToDoText: ""
+        })
+
+    toggleTodo = (toDo: ToDo) => this.setState({
+        toDos: this.state.toDos.map(it => it.text == toDo.text ? { ...it, isDone: !it.isDone } : it)
+    })
+
+    showToDoRows = () => this.state.toDos.map(toDo =>
+        <tr>
+            <td>{toDo.text}</td>
+            <td><input type="checkbox" checked={toDo.isDone} onChange={() => this.toggleTodo(toDo)}/></td>
+        </tr>
+    )
+
     render = () =>
         <div>
             <h4 className="bg-primary text-white text-center p-2">
-                To-do List
+                {this.state.username}'s To-do List
+                ({this.state.toDos.filter(t => !t.isDone).length} left)
             </h4>
+            <div className="container-fluid">
+                <input type="text" className="form-control" value={this.state.newToDoText}
+                       onChange={e => this.setState({ newToDoText: e.target.value })} placeholder="Something to do..."/>
+                <button className="btn btn-primary m2" onClick={this.createNewTodo}>
+                    Add
+                </button>
+            </div>
+            <table className="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Done</th>
+                </tr>
+                </thead>
+                <tbody>{this.showToDoRows()}</tbody>
+            </table>
         </div>
 }

8cbcfc07bf1d264c50f4bf78225512f1a7485460
Alexandre Gressier <alexandre@gressier.dev>
2021-05-06 22:58:22 +0200

    Refactor the App component into a function component

--- front-end/web/todo-web-app/src/App.tsx
+++ front-end/web/todo-web-app/src/App.tsx
@@ -1,52 +1,39 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import { ToDo } from "./ToDo"
 import { ToDoBanner } from "./ToDoBanner"
 import { ToDoRow } from "./ToDoRow"
 import { ToDoCreator } from "./ToDoCreator"
 
-export default class App extends Component<{}, { username: string, toDos: ToDo[] }> {
+export const App = () => {
+    const [username,] = useState("Alex")
+    const [toDos, setTodos] = useState([
+        { text: "Buy Flowers", isDone: false },
+        { text: "Get Shoes", isDone: false },
+        { text: "Collect Tickets", isDone: true },
+        { text: "Call Joe", isDone: false },
+    ])
 
-    constructor(props: {}) {
-        super(props)
-        this.state = {
-            username: "Alex",
-            toDos: [
-                { text: "Buy Flowers", isDone: false },
-                { text: "Get Shoes", isDone: false },
-                { text: "Collect Tickets", isDone: true },
-                { text: "Call Joe", isDone: false },
-            ]
-        }
-    }
+    const createNewTodo = (toDoText: string) =>
+        toDoText && !toDos.find(it => it.text === toDoText) && setTodos([...toDos, { text: toDoText, isDone: false }])
 
-    createNewTodo = (toDoText: string) =>
-        toDoText && !this.state.toDos.find(toDo => toDo.text === toDoText) && this.setState({
-            toDos: [...this.state.toDos, { text: toDoText, isDone: false }]
-        })
+    const toggleTodo = (toDo: ToDo) =>
+        setTodos(toDos.map(it => it.text === toDo.text ? { ...it, isDone: !it.isDone } : it))
 
-    toggleTodo = (toDo: ToDo) =>
-        this.setState({
-            toDos: this.state.toDos.map(it => it.text === toDo.text ? { ...it, isDone: !it.isDone } : it)
-        })
-
-    render = () =>
-        <div>
-            <ToDoBanner username={this.state.username} toDos={this.state.toDos}/>
-            <div className="container-fluid">
-                <ToDoCreator onAdd={this.createNewTodo}/>
-                <table className="table table-striped table-bordered">
-                    <thead>
-                    <tr>
-                        <th>Description</th>
-                        <th>Done</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {this.state.toDos.map(toDo =>
-                        <ToDoRow key={toDo.text} toDo={toDo} onChange={this.toggleTodo}/>
-                    )}
-                    </tbody>
-                </table>
-            </div>
+    return <div>
+        <ToDoBanner username={username} toDos={toDos}/>
+        <div className="container-fluid">
+            <ToDoCreator onAdd={createNewTodo}/>
+            <table className="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Done</th>
+                </tr>
+                </thead>
+                <tbody>
+                {toDos.map(it => <ToDoRow key={it.text} toDo={it} onChange={toggleTodo}/>)}
+                </tbody>
+            </table>
         </div>
+    </div>
 }

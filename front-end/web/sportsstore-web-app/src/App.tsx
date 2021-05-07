d7d0111f7adf5139218d3b795a0e967b51b7894c
Alexandre Gressier <alexandre@gressier.dev>
2021-05-07 23:36:07 +0200

    Install npm packages for the Sports Store web app

--- front-end/web/sportsstore-web-app/src/App.tsx
+++ front-end/web/sportsstore-web-app/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 
-export default class App extends Component {
+export class App extends Component {
     render = () =>
         <div>
             <h4 className="bg-primary text-white text-center p-2">

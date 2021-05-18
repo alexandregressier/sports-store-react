237a4cb58a8d0046e5e88967da626963bdfb9615
Alexandre Gressier <alexandre@gressier.dev>
2021-05-18 22:16:35 +0200

    Define domain objects

--- front-end/web/sportsstore-web-app/src/redux/domain.ts
+++ front-end/web/sportsstore-web-app/src/redux/domain.ts
@@ -1,12 +1,30 @@
-export enum DataType {
-    PRODUCTS = "products",
-    CATEGORIES = "categories",
+export interface Person {
+    fullName: string,
+    emailAddress: string,
 }
 
+export interface Place {
+    street: string,
+    city: string,
+    postalCode: string,
+    country: string,
+}
+
+export type Data = Product | Order
+
 export interface Product {
+    kind: "product",
     id: number,
     name: string,
+    price: number,
     category: string,
     description: string,
-    price: number,
+}
+
+export interface Order {
+    kind: "order",
+    id: number,
+    buyer: Person
+    deliveryAddress: Place,
+    products: Product
 }

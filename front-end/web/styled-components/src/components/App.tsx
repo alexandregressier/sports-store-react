df6baf37c5d2b98359ca40c7b0e129775ab4c567
Alexandre Gressier <alexandre@gressier.dev>
2021-05-21 22:40:27 +0200

    Define the global style

--- front-end/web/styled-components/src/components/App.tsx
+++ front-end/web/styled-components/src/components/App.tsx
@@ -1,8 +1,22 @@
 import React from "react"
 import { Button } from "components/common"
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+      min-height: 100vh;
+      margin: 0;
+
+      color: black;
+      background-color: white;
+
+      font-family: "Kaushan Script", serif;
+  }
+`
 
 export const App = () =>
     <>
+        <GlobalStyle/>
         <h1>styled-components</h1>
         <Button>Yes</Button>
     </>
